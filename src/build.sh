e0cc12780990373bbad1fcb24eb8e8218b2179d3
Johan Jonkers <johan@seecr.nl>
2015-10-30 16:35:01 +0100

    JJ: fixes for rh7 packaging

--- src/build.sh
+++ src/build.sh
@@ -32,16 +32,20 @@ if [ -z "$libDir" ]; then
     libDir=$(dirname $mydir)/lib
 fi
 
-rm -rf $buildDir
-mkdir $buildDir
-rm -rf $libDir
-mkdir -p $libDir
+rm -rf $buildDir $libDir
+mkdir --parents buildDir $libDir
+
+pythonVersion=$(python --version 2>&1 | awk '{print $2}' | cut -d. -f-2)
 
 javac=/usr/lib/jvm/java-1.7.0-openjdk.x86_64/bin/javac
-luceneJarDir=/usr/lib64/python2.6/site-packages/lucene
+if [ ! -f "$javac" ]; then
+    javac=/usr/lib/jvm/java-1.7.0/bin/javac
+fi
+
+luceneJarDir=/usr/lib64/python${pythonVersion}/site-packages/lucene
 if [ -f /etc/debian_version ]; then
     javac=/usr/lib/jvm/java-7-openjdk-amd64/bin/javac
-    luceneJarDir=/usr/lib/python2.7/dist-packages/lucene
+    luceneJarDir=/usr/lib/python${pythonVersion}/dist-packages/lucene
 fi
 
 LUCENE_VERSION=4.10.1
@@ -61,15 +65,12 @@ python -m jcc.__main__ \
     --build \
     --install
 
-rootLibDir=$mydir/root/usr/lib64/python2.6/site-packages/meresco_sequentialstore
+rootLibDir=$mydir/root/usr/lib64/python${pythonVersion}/site-packages/meresco_sequentialstore
 if [ -f /etc/debian_version ]; then
-    rootLibDir=$mydir/root/usr/local/lib/python2.7/dist-packages/meresco_sequentialstore
+    rootLibDir=$mydir/root/usr/local/lib/python${pythonVersion}/dist-packages/meresco_sequentialstore
 fi
 
 mv ${rootLibDir} $libDir/
 
-
-rm -rf $buildDir
-rm -rf $mydir/root
-rm -rf $mydir/meresco_sequentialstore.egg-info
+rm -rf $buildDir  $mydir/root $mydir/meresco_sequentialstore.egg-info
 
